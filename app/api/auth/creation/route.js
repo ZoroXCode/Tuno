beacea1bfcbe67c8f3bed824712f6c0c6b7b355f
ZoroXCode <samirmc8384@outlook.com>
2024-12-16 19:59:35 +0600

    done

--- app/api/auth/creation/route.js
+++ app/api/auth/creation/route.js
@@ -28,5 +28,5 @@ export async function GET() {
     });
   }
 
-  return NextResponse.redirect("http://localhost:3000/shop/");
+  return NextResponse.redirect("https://tuno-smoky.vercel.app/shop");
 }
